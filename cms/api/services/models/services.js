1d9d1717bf0264ff435ed62bfd46e1051f926a05
redesign <3030.doc@gmail.com>
2021-05-19 13:01:59 +0300

    frontend reloaders

--- cms/api/services/models/services.js
+++ cms/api/services/models/services.js
@@ -5,4 +5,33 @@
  * to customize this model
  */
 
-module.exports = {};
+const restartPM=function(){
+	const { exec } = require("child_process");
+	exec("pm2 restart frontend", (error, stdout, stderr) => {
+	    if (error) {
+	        strapi.log.debug(`error: ${error.message}`)
+	        return;
+	    }
+	    if (stderr) {
+	        strapi.log.debug(`stderr: ${stderr}`)
+	        return;
+	    }
+        strapi.log.debug(`stdout: ${stdout}`)
+
+	});
+}
+
+
+module.exports = {
+	lifecycles: {
+	    async afterUpdate(result, params, data) {
+	    	restartPM()
+	    },
+	    async afterCreate(result, params, data) {
+	    	restartPM()
+	    },
+	    async afterDelete(result, params, data) {
+	    	restartPM()
+	    },
+   },
+};
